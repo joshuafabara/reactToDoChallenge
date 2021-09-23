b3859bf00b90d2dc5d0f96a495ab5c933bf6dfe4
Joshua Fabara <joshuafabara@gmail.com>
2021-09-23 00:44:36 -0500

    Added tablet format for users and order button for name

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,20 +1,22 @@
 import axios from 'axios'
-import React, { useEffect, useState, Fragment } from 'react'
+import React, { useEffect, useState, Fragment, useRef } from 'react'
 
 const UsersList = () => {
   const [users, setUsers] = useState([]);
+  const usersRef = useRef(null);
 
   const getData = () => {
-    console.log('getting data');
+    // console.log('getting data');
     axios.get('http://jsonplaceholder.typicode.com/users').then(res => {
       console.table(res.data)
+      usersRef.current = res.data;
       setUsers(res.data);
     })
   };
 
   const userItems = users.map((user) =>
-    <Fragment>
-      <tr key={user.id}>
+    <Fragment key={user.id}>
+      <tr>
         <td>{user.name}</td>
         <td>{user.username}</td>
         <td>{user.email}</td>
@@ -22,8 +24,24 @@ const UsersList = () => {
     </Fragment>
   );
 
-  // getData();
+  const orderByName = () => {
+    console.log('will order by Name');
+    let newUsers = [...users];
+    newUsers.sort(function(a, b) {
+      var textA = a.name.toUpperCase();
+      var textB = b.name.toUpperCase();
+      return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    });
+    console.log('newUsers');
+    console.log(newUsers);
+    usersRef.current = newUsers;
+    setUsers(newUsers);
+  }
+
   useEffect(() => {
+    if (users && users === usersRef.current) {
+      return;
+    }
     console.log('getting effect use');
     getData();
   });
@@ -32,12 +50,23 @@ const UsersList = () => {
     <div className="users-list">
       Users List page!
       <table>
-        <tr>
-          <th>Name</th>
-          <th>Username</th>
-          <th>Email</th>
-        </tr>
-        {userItems}
+        <thead>
+          <tr>
+            <th>
+              Name
+              <button onClick={orderByName}>Order</button>
+            </th>
+            <th>
+              Username
+            </th>
+            <th>
+              Email
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {userItems}
+        </tbody>
       </table>
     </div>
   );
